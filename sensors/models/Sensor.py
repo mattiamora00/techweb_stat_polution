4fc24becc79541fc59ec40fbadf0004f7d448b84
mattiamora00 <mattia.m@wegloo.it>
2023-06-04 13:25:01 +0200

    add str method

--- sensors/models/Sensor.py
+++ sensors/models/Sensor.py
@@ -7,11 +7,9 @@ class Sensor(models.Model):
     lng = models.FloatField()
     city = models.ForeignKey("geo.City", on_delete=models.CASCADE)
     pollutant = models.ForeignKey("pollutants.Pollutant", on_delete=models.CASCADE)
-    date_change_filter = models.DateField(blank=True,null=True)
-    oxidation_level = models.FloatField(blank=True,null=True)
-    sensor_model=models.CharField(max_length=30)
-
+    date_change_filter = models.DateField(blank=True, null=True)
+    oxidation_level = models.FloatField(blank=True, null=True)
+    sensor_model = models.CharField(max_length=30)
 
     def __str__(self):
         return f"{self.sensor_code} {self.city.name} {self.pollutant.name} {self.sensor_model}"
-
