723bdc7e4ee334ae248bd6d9ee4317aa06069243
mattiamora00 <mattia.m@wegloo.it>
2023-06-04 13:51:15 +0200

    add str method

--- pollutants/schemas/Pollutant.py
+++ pollutants/schemas/Pollutant.py
@@ -3,6 +3,9 @@ import graphene
 from graphene_django import DjangoObjectType
 from graphene import relay, ObjectType
 from ..models.Pollutant import Pollutant
+from geo.models.City import City
+from sensors.models.Sensor import Sensor
+from sensors.models.Rilevation import Rilevation
 from ..forms.Pollutant import PollutantForm
 from graphene_django.forms.mutation import DjangoFormMutation
 from graphene_django.forms.mutation import DjangoModelFormMutation
@@ -27,6 +30,26 @@ class PollutantMutation(DjangoFormMutation):
 class Query(graphene.ObjectType):
     pollutants = graphene.List(PollutantType,name=graphene.String(),threshold=graphene.Float())
     pollutant = graphene.Field(PollutantType, id=graphene.Int())
+    pollutants_of_city = graphene.List(PollutantType,city=graphene.String())
+
+    @staticmethod
+    def resolve_pollutants_of_city(self,info,**kwargs):
+        city=kwargs["city"]
+        city_id=City.objects.get(name=city).id
+        sensors_list=Sensor.objects.filter(city_id=city_id)
+        dataSet = {}
+        for sensor in sensors_list:
+            rilevation_list = [rilevation  for rilevation in Rilevation.objects.filter(sensor_id=sensor.id)]
+            pollutant_name=sensor.pollutant.name
+            if sensor.pollutant.name in dataSet:
+                dataSet[sensor.pollutant.name].append(rilevation_list)
+            else:
+                new_pollutant=[]
+                new_pollutant.append("s")
+                dataSet[pollutant_name]=new_pollutant
+        res=[]
+        res.append(dataSet)
+        return res
 
     @staticmethod
     def resolve_pollutants(self, info, **kwargs):
