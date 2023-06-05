c3361174290f9c915e0eb892c1038568494ebb09
mattiamora00 <mattia.m@wegloo.it>
2023-06-05 22:59:05 +0200

    bug fix min validator for password

--- users/models/User.py
+++ users/models/User.py
@@ -1,10 +1,10 @@
-from django.core.validators import MinValueValidator
+from django.core.validators import MinLengthValidator
 from django.db import models
 
 class User(models.Model):
     username=models.CharField(max_length=30)
     email=models.EmailField()
-    password=models.CharField(max_length=64,validators=[MinValueValidator(64)])
+    password=models.CharField(max_length=64,validators=[MinLengthValidator(64)])
     view_sensor=models.BooleanField(default=False)
     view_plan=models.BooleanField(default=False)
     view_graph=models.BooleanField(default=False)
