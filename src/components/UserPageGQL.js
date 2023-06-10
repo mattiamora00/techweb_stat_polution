93d6a73c837eec470d43b906e7f579c1021025dc
mattiamora00 <mattia.m@wegloo.it>
2023-06-10 17:10:04 +0200

    fix

--- src/components/UserPageGQL.js
+++ src/components/UserPageGQL.js
@@ -1,11 +1,5 @@
 import { gql } from '@apollo/client';
 
-const GET_USER=gql`
-query PollutantOfCity($city:String!){
-  pollutantsOfCity(city:$city)
-}
-`
-
 const UPDATE_USER=gql`
 mutation User($id:ID,$username:String!,$email:String!,$password:String!,$imageProfile:String,$viewPlan:Boolean,$viewGraph:Boolean,$viewSick:Boolean,$viewSensor:Boolean){
   upsertUser(input:{id:$id,username:$username,email:$email,password:$password,profileImage:$imageProfile,viewPlan:$viewPlan,viewSick:$viewSick,viewGraph:$viewGraph,viewSensor:$viewSensor}){
@@ -21,4 +15,4 @@ mutation User($id:ID,$username:String!,$email:String!,$password:String!,$imagePr
 }
 `
 
-export {GET_USER,UPDATE_USER};
+export {UPDATE_USER};
